a2b0b627ea47bf4f42974a12dd77eba150da0335
Nicolas Thierry <nicolas.thierry.perso@gmail.com>
2022-11-06 22:06:07 +0100

    loss fct and numerical differentiation

--- dense_neural_network.py
+++ dense_neural_network.py
@@ -34,6 +34,37 @@ class Activation_Softmax :
         
         self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
+# Common loss class
+class Loss :
+    # Calculates the data and regularization losses
+    # given model output and ground truth values
+    def calculate ( self , output , y ):
+        # Calculate sample losses
+        sample_losses = self.forward(output, y)
+        # Calculate mean loss
+        data_loss = np.mean(sample_losses)
+        # Return loss
+        return data_loss
+
+# Cross-entropy loss
+class Loss_CategoricalCrossentropy ( Loss ):
+    # Forward pass
+    def forward ( self , y_pred , y_true ):
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7 , 1 - 1e-7 )
+            
+        # Probabilities for target values -
+        # only if categorical labels
+        if len (y_true.shape) == 1 :
+            correct_confidences = y_pred_clipped[range ( len (y_pred_clipped)), y_true]
+        # Mask values - only for one-hot encoded labels
+        elif len (y_true.shape) == 2 :
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis = 1)
+            
+        neg_log = - np.log(correct_confidences)
+        return neg_log
+        
 dense1 = Layer_Dense(2 , 3)
 activation1 = Activation_ReLU()
 
@@ -49,4 +80,15 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
 print (activation2.output[:5])
-#chap5 p.107
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+print ('loss:', loss)
+
+# Calculate accuracy from output of activation2 and targets
+predictions = np.argmax(activation2.output, axis = 1)
+if len (y.shape) == 2 :
+    y = np.argmax(y, axis = 1 )
+    
+accuracy = np.mean(predictions == y)
+print ( 'acc:' , accuracy)
